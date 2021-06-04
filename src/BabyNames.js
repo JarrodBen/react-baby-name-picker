ff68da54bfff6bf8499b5cb089c2945b5f337b55
South Africa - Jarrod Benjamin <46967203+JarrodBen@users.noreply.github.com>
2021-06-04 12:49:04 +0200

    Completed BabyNames, moving on to the next

--- src/BabyNames.js
+++ src/BabyNames.js
@@ -1,3 +1,44 @@
 import React, { useState } from "react";
 
+function BabyNames({ babyList }) {
+
+    const babyFilterList = babyList
+  
+    const sortedNames = babyFilterList
+      .sort((name1, name2) => name1.name.localeCompare(name2.name));
+    
+    const favouriteBaby = (name) => console.log(name.target);
+    
+    return (
+      <div>
+        <h3>Favourites</h3>
+        <div style={{ display: "flex", flexWrap: "wrap", height: "fit-content", border: ".2rem solid white" }}>
+          {sortedNames
+            .map((babyName) => {
+  
+              let color = babyName.sex === "f" ? "#cf1760" : "#10ebdc";
+  
+              return (
+                <p style={
+                  {
+                    padding: "8px",
+                    margin: "3.2px",
+                    width: "fit-content",
+                    backgroundColor: color,
+                    textAlign: "center"
+                  }}
+                  key={babyName.id}
+                  onClick={favouriteBaby}
+                >
+                  {babyName.name}
+                </p>
+              )
+            }
+            )
+          }
+        </div>
+  
+      </div>)
+  }
+
 export default BabyNames
