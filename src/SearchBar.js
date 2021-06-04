cd243dfeddd6ebf6eddada98467eb5c9c77262b8
South Africa - Jarrod Benjamin <46967203+JarrodBen@users.noreply.github.com>
2021-06-04 12:24:48 +0200

    Finished up SearchBar, moving on to the next

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,3 +1,16 @@
 import React from "react"
 
+const SearchBar = ({ keyword, onChange:setKeyword }) => {
+    const BarStyling = { width: "320px", background: "#ffffff", border: "none", padding: "8px" };
+    return (
+      <input
+        style={BarStyling}
+        key="random1"
+        value={keyword}
+        placeholder={"Search for a baby name"}
+        onChange={(e) => setKeyword(e.target.value)}
+      />
+    );
+  }
+
 export default SearchBar
