515bd2ff81c75926dd4b2277e4bda5bdae30c79c
South Africa - Jarrod Benjamin <46967203+JarrodBen@users.noreply.github.com>
2021-06-04 13:29:36 +0200

    Completed up until Level 3
    - Will make any necessary changes forward

--- src/App.js
+++ src/App.js
@@ -7,24 +7,30 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [input, setInput] = useState('');
+  const [babyList, setBabyList] = useState(BabyNamesData);
+  
+  const updateInput = (input) => {
+    const newBaby = BabyNamesData
+    const filtered = newBaby.filter(baby => {
+      return (
+        baby.name.toLowerCase().includes(input.toLowerCase())
+      )
+    })
+    setInput(input);
+    setBabyList(filtered);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>Baby Names Picker</h3>
+        <SearchBar input={input} onChange={updateInput} />
+        <BabyNames babyList={ babyList }/>
       </header>
     </div>
-  );
+  )
 }
 
 export default App;
